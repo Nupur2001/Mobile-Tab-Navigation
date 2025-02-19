d35e0f9662b08a72a3c7039c7648f02eb7272e7d
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-02-19 17:34:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  let navUlLi = document.querySelectorAll("nav ul li");
+  let imgs = document.querySelectorAll("img");
 
-})
+  navUlLi.forEach((e) => {
+    e.addEventListener("click", () => {
+      // let eachLi=document.createElement('li')
+      e.classList.add("active");
+    });
+  });
+});
